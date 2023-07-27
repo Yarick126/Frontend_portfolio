165d34c28f19196f226c3b17155db4d5b55eb6f7
Yarick126 <gadzhikurbanovy@mail.ru>
2023-07-27 17:26:05 +0300

    Thanks for coming

--- src/components/screens/book-me/book-me.component.js
+++ src/components/screens/book-me/book-me.component.js
@@ -4,6 +4,8 @@ import template from './book-me.template.html'
 import styles from './book-me.module.scss'
 import { BaseScreen } from '@/core/components/base-screen.component'
 import { Heading } from '@/components/ui/heading/heading.component'
+import { $R } from '@/core/rquery/rquery.lib'
+import { InputField } from '@/components/ui/input-field/input-field.component'
 
 
 export class BookMe extends BaseScreen {
@@ -17,6 +19,30 @@ export class BookMe extends BaseScreen {
       new Heading('Lets get in touch')
     ], styles)
     
+    $R(this.element).find('#input-fields').append(
+      new InputField({
+        inputName:'MY NAME:'
+      }).render()
+    )
+    .append(
+      new InputField({
+        inputName:'TYPE OF BOOKING:'
+      }).render()
+    )
+    .append(
+      new InputField({
+        type: 'email',
+        inputName:'EMAIL:'
+      }).render()
+    )
+    .append(
+      new InputField({
+        type:'number',
+        inputName:'PHONE NUMBER:'
+      }).render()
+    )
+
+
     return this.element
   }
 }
